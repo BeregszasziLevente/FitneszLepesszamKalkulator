4dd6392eb0959a1f74b9b02b469ab3dd5e68f566
BeregszasziLevente <beregszaszi.levente.2024d@telekimezotur.hu>
2026-09-24 13:07:57 +0200

    2. feladat

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+List<int> lepes = new List<int> ();
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"{i+1}. nap lépésszám: ");
+    lepes.Add(int.Parse(Console.ReadLine()));
+}
+
